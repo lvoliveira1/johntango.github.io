354f04ecf52c2e19899120f6ee6bc18b3369d8fe
John <jrw@mit.edu>
2021-09-27 12:23:24 -0400

    Teach01

---
+++ assets/docs/teaching.js
@@ -0,0 +1,13 @@
+var courses = `
+  <div class="sectionHead">
+      <h1>JOOOOOOHHHHNN Courses</h1>
+  </div>               
+  <p>          
+`;
+
+
+function myfunction() {
+    console.log('function ping!');
+}
+
+console.log('done');
